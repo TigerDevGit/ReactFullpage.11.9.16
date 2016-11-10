5717713a0ea1e8a30727bf54f2b66daabef863c0
TigerDevGit <niklasla220@gmail.com>
2016-11-10 12:47:43 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-let arr = new Array(10);
-arr.fill(1);
-let comarr = arr.map((entity,i)=>(<App key={i}/>));
-console.log(JSON.stringify(comarr));
 ReactDOM.render(
   <div>
     <App count='50' oneColum='5'/>
